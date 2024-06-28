018eadd9981c4cde3857b54090d7de339d831b12
DANGO <dahianaprogramacion@gmail.com>
2024-06-27 21:53:54 -0300

    funcion js click

--- ToDoList/js/script.js
+++ ToDoList/js/script.js
@@ -3,6 +3,7 @@ const addBtn = document.querySelector(".btn-add");
 const ul = document.querySelector("ul");
 const empty = document.querySelector(".empty");
 
+/*funcion del boton de agregar*/
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -22,6 +23,8 @@ addBtn.addEventListener("click", (e) => {
   }
 });
 
+
+/*funcion del boton de borrar*/
 function addDeleteBtn() {
   const deleteBtn = document.createElement("button");
 
@@ -41,3 +44,17 @@ function addDeleteBtn() {
 
   return deleteBtn;
 }
+
+
+
+/*funcion para el verificador de tareas completadas*/
+/*se puede determinar si el elemento se selecciono o no*/
+/*si se selecciona el elemento (click), se muestra un alert */
+
+function verificar() {
+
+if (document.getElementById('click').checked) {
+  alert("Has cumplido con todas tus tareas por hoy");
+}
+}
+  
